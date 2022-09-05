ff791c8c91a7705ba1a29ebd822f236e917f3be2
Michael Kuhlmann <kuhlmann@nt.uni-paderborn.de>
2022-09-05 15:26:52 +0200

    Add missing packages

--- setup.py
+++ setup.py
@@ -5,8 +5,15 @@ from setuptools import find_packages
 setup(
     name='unsup_seg',
     version='0.0',
-    description='',
+    description='Forked from https://github.com/felixkreuk/UnsupSeg',
     author='',
     author_email='',
     license='',
+    packages=find_packages(),
+    install_requires=[
+        'dill',
+        'boltons',
+        'librosa',
+        'torchaudio',
+    ],
 )
